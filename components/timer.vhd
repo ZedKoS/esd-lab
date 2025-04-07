9f2722dac711b10b4ab8d752959eb1b0626f2906
Marco Spataro <marcospataro03@gmail.com>
2025-04-07 16:15:39 +0200

    Rendi timer indipendente

--- components/timer.vhd
+++ components/timer.vhd
@@ -15,49 +15,27 @@ entity Timer is
 end entity Timer;
 
 architecture Behavior of timer is
-	component CounterN is
-        generic (N : natural);
-        port
-        (
-            Enable, Clock : in std_logic;
-            AsyncReset : in std_logic := '1';
-            DataLoad : in unsigned(N-1 downto 0) := to_unsigned(0, N);
-            Load : in std_logic;
-            Overflow : out std_logic;
-            Count : buffer unsigned(N-1 downto 0)
-        );			
-	end component;
-
-    component Reg is
-        generic (N : natural);
-        port
-        (
-            DataIn : in std_logic_vector(N-1 downto 0);
-            Clock : in std_logic;
-			Enable : in std_logic;
-            AsyncReset : in std_logic := '0';
-            SyncReset  : in std_logic := '0';
-            DataOut : out std_logic_vector(N-1 downto 0)
-        );
-    end component;
-
     signal loaded_end_count : unsigned(N-1 downto 0);
-    signal restart : std_logic;
-
 begin
-    SyncEndCount: Reg
-        generic map (N => N)
-        port map (Enable => LoadEndCount, Clock => Clock,
-            SyncReset => '0', AsyncReset => AsyncReset,
-			DataIn => std_logic_vector(EndCount), unsigned(DataOut) => loaded_end_count);
-
     Done <= '1' when Count = loaded_end_count else '0';
     Wrap <= Done and Enable;
-    restart <= Wrap or LoadEndCount;
 
-    Counter: CounterN
-        generic map (N => N)
-        port map (Enable => Enable, Clock => Clock, AsyncReset => AsyncReset,
-            Load => restart, DataLoad => to_unsigned(0, N), Overflow => open, Count => Count);
+    TIMER_PROC: process(Clock, AsyncReset)
+    begin
+        if AsyncReset = '1' then
+            loaded_end_count <= to_unsigned(0, N);
+            Count <= to_unsigned(0, N);
 
+        elsif rising_edge(Clock) then
+            if (Wrap or LoadEndCount) = '1' then
+                Count <= to_unsigned(0, N);
+                
+                if LoadEndCount = '1' then
+                    loaded_end_count <= EndCount;
+                end if;
+            elsif Enable = '1' then
+                Count <= Count + 1;
+            end if;
+        end if;
+    end process TIMER_PROC;
 end architecture Behavior;
