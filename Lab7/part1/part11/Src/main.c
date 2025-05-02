5387b73325cd7df3f4586bf76b72dd1bbcda1ee4
Davide Covolo <164493065+Disteroas@users.noreply.github.com>
2025-05-02 11:50:36 +0200

    Part1 worka godo

--- Lab7/part1/part11/Src/main.c
+++ Lab7/part1/part11/Src/main.c
@@ -1,16 +1,17 @@
-#include <stdint.h>
-
 int main(void)
 {
-	int i;
+
  //PORT REGISTERS
- volatile unsigned int *GPIOA_MODER = (unsigned int*) (0x40020000 + 0x00);
- volatile unsigned int *GPIOA_ODR = (unsigned int*) (0x40020000 + 0x14);
- volatile unsigned int *GPIOC_MODER = (unsigned int*) (0x40020000 + 0x0800 + 0x00);
- volatile unsigned int *GPIOC_PUPDR = (unsigned int*) (0x40020000 + 0x0800 + 0x0C);
- volatile unsigned int *GPIOC_IDR = (unsigned int*) (0x40020000 + 0x0800 + 0x10);
+
+ volatile unsigned int* GPIOA_MODER = (unsigned int*) (0x40020000 + 0x00);
+ volatile unsigned int* GPIOA_ODR = (unsigned int*) (0x40020000 + 0x14);
+ volatile unsigned int* GPIOC_MODER = (unsigned int*) (0x40020000 + 0x0800 + 0x00);
+ volatile unsigned int* GPIOC_PUPDR = (unsigned int*) (0x40020000 + 0x0800 + 0x0C);
+ volatile unsigned int* GPIOC_IDR = (unsigned int*) (0x40020000 + 0x0800 + 0x10);
+
  //CLOCK REGISTERS
- volatile unsigned int *RCC_AHB1ENR = (unsigned int*) (0x40023800 + 0x30);
+ volatile unsigned int* RCC_AHB1ENR = (unsigned int*) (0x40023800 + 0x30);
+
  //VARIABLES
  //ENABLE PORT CLOCK:
  // this ensure that the peripheral is enabled and connected to the AHB1 bus
@@ -18,20 +19,24 @@ int main(void)
  *RCC_AHB1ENR |= 0x04U;
  //CONFIGURE PORT: set MODER[11:10] = 0x1
  *GPIOA_MODER |= 0x400;
- //CONFIGURE PORT: set MODER[13:12] = 0x1
- *GPIOC_MODER |= 0x2000;
- //CONFIGURE PORT: set P13 to PULLDOWN
- *GPIOC_PUPDR |= 0x4000;
+ //CONFIGURE PORT: set MODER[27:26] = 0x1
+ *GPIOC_MODER &= ~0xC000000;
+ //CONFIGURE PORT: set P13 to PULLUP
+ *GPIOC_PUPDR |= 0x4000000;
  //SWITCH ON THE LED: set ODR[5] = 0x1, that is pulls PA5 high
- *GPIOA_ODR = *GPIOA_ODR | 0x20;
+ *GPIOA_ODR |= 0x20;
  // Application code (Infinite loop)
+	
  while (1)
  {
+	 int push_status = (*GPIOC_IDR >> 13) & 0x1;
+	 if(push_status == 1){
+		 *GPIOA_ODR &= ~(0x20); //complements bit => toggles LED
+	
+	 } else{
+	
+		 *GPIOA_ODR |= (0x20); //complements bit => toggles LED
+	 }
 
-	if(*GPIOC_IDR > 38000){
-		*GPIOA_ODR = *GPIOA_ODR ^ 0x20; //complements bit => toggles LED
-		}
-	for(i=0; i<130000; i++){ //time for click
-	}
  }
 }
