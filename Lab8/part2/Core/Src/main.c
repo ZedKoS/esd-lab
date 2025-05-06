f942250882324e8702bc7e2223a2dbf16f22774a
LeaplusjZer0 <crisafi.alessandro@gmail.com>
2025-05-06 11:49:19 +0200

    part1, part2, part3 lab8

--- Lab8/part2/Core/Src/main.c
+++ Lab8/part2/Core/Src/main.c
@@ -31,7 +31,8 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+#define HALF_PERIOD1 16800 // 2.5 kHz
+#define HALF_PERIOD2 3360 // 12.5 kHz
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -99,22 +100,31 @@ int main(void) {
 	SysTick_Config(SystemCoreClock / 1000);
 	LL_TIM_WriteReg(TIM3, CR1, LL_TIM_ReadReg(TIM3,CR1) | 0x1); // enable counter
 	LL_TIM_WriteReg(TIM3, CNT, 0); // initialize counter
-	int EndCNT = 16800;
 	/* USER CODE END 2 */
 
 	/* Infinite loop */
 	/* USER CODE BEGIN WHILE */
 	while (1) {
-		int CNTStatus = LL_TIM_ReadReg(TIM3, CNT);
-		if (CNTStatus >= EndCNT) {
-			LL_GPIO_WriteReg(GPIOA, ODR,
-					LL_GPIO_ReadReg(GPIOA, ODR) ^ (1U << 10)); // toggles output
-			LL_TIM_WriteReg(TIM3, SR, LL_TIM_ReadReg(TIM3,SR) & ~(0x3)); // clear flags
-			LL_TIM_WriteReg(TIM3, CNT, 0); // resets counter
-		}
 		/* USER CODE END WHILE */
-
 		/* USER CODE BEGIN 3 */
+		int StatR = LL_TIM_ReadReg(TIM3, SR);
+		int cc1if = (StatR >> 1) & 0x1; //Ch1
+		int cc2if = (StatR >> 2) & 0x1; //Ch2
+		if (cc1if == 1) {
+			LL_TIM_WriteReg(TIM3, SR, LL_TIM_ReadReg(TIM3,SR) & ~(0b010)); // clear flag
+			LL_GPIO_WriteReg(GPIOA, ODR,
+					LL_GPIO_ReadReg(GPIOA, ODR) ^ (1U << 10)); // toggle output
+			LL_TIM_WriteReg(TIM3, CCR1,
+					LL_TIM_ReadReg(TIM3, CCR1) + HALF_PERIOD1); //TC1 += HALF_PERIOD1
+		}
+		if (cc2if == 1) {
+			LL_TIM_WriteReg(TIM3, SR, LL_TIM_ReadReg(TIM3,SR) & ~(0b100)); // clear flag
+			LL_GPIO_WriteReg(GPIOB, ODR,
+					LL_GPIO_ReadReg(GPIOB, ODR) ^ (1U << 10)); // toggle output
+			LL_TIM_WriteReg(TIM3, CCR2,
+					LL_TIM_ReadReg(TIM3, CCR2) + HALF_PERIOD2); //TC2 += HALF_PERIOD2
+		}
+
 	}
 	/* USER CODE END 3 */
 }
@@ -181,18 +191,20 @@ static void MX_TIM3_Init(void) {
 	/* USER CODE END TIM3_Init 1 */
 	TIM_InitStruct.Prescaler = 0;
 	TIM_InitStruct.CounterMode = LL_TIM_COUNTERMODE_UP;
-	TIM_InitStruct.Autoreload = 33600;
+	TIM_InitStruct.Autoreload = 65535;
 	TIM_InitStruct.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
 	LL_TIM_Init(TIM3, &TIM_InitStruct);
 	LL_TIM_DisableARRPreload(TIM3);
 	LL_TIM_SetClockSource(TIM3, LL_TIM_CLOCKSOURCE_INTERNAL);
 	TIM_OC_InitStruct.OCMode = LL_TIM_OCMODE_FROZEN;
-	TIM_OC_InitStruct.OCState = LL_TIM_OCSTATE_DISABLE;
+	TIM_OC_InitStruct.OCState = LL_TIM_OCSTATE_ENABLE;
 	TIM_OC_InitStruct.OCNState = LL_TIM_OCSTATE_DISABLE;
 	TIM_OC_InitStruct.CompareValue = 0;
 	TIM_OC_InitStruct.OCPolarity = LL_TIM_OCPOLARITY_HIGH;
 	LL_TIM_OC_Init(TIM3, LL_TIM_CHANNEL_CH1, &TIM_OC_InitStruct);
 	LL_TIM_OC_DisableFast(TIM3, LL_TIM_CHANNEL_CH1);
+	LL_TIM_OC_Init(TIM3, LL_TIM_CHANNEL_CH2, &TIM_OC_InitStruct);
+	LL_TIM_OC_DisableFast(TIM3, LL_TIM_CHANNEL_CH2);
 	LL_TIM_SetTriggerOutput(TIM3, LL_TIM_TRGO_RESET);
 	LL_TIM_DisableMasterSlaveMode(TIM3);
 	/* USER CODE BEGIN TIM3_Init 2 */
@@ -213,10 +225,22 @@ static void MX_GPIO_Init(void) {
 	/* USER CODE END MX_GPIO_Init_1 */
 
 	/* GPIO Ports Clock Enable */
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
 	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
 
 	/**/
+	LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_10);
+
+	/**/
 	LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_10);
+
+	/**/
+	GPIO_InitStruct.Pin = LL_GPIO_PIN_10;
+	GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+	GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+	GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+	GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+	LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 	/**/
 	GPIO_InitStruct.Pin = LL_GPIO_PIN_10;
--- Lab8/part3/Core/Src/main.c
+++ Lab8/part3/Core/Src/main.c
@@ -31,7 +31,8 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+#define HALF_PERIOD1 16800 // 2.5 kHz
+#define HALF_PERIOD2 3360 // 12.5 kHz
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -65,7 +66,6 @@ static void MX_TIM3_Init(void);
 int main(void) {
 
 	/* USER CODE BEGIN 1 */
-
 	/* USER CODE END 1 */
 
 	/* MCU Configuration--------------------------------------------------------*/
@@ -99,22 +99,30 @@ int main(void) {
 	SysTick_Config(SystemCoreClock / 1000);
 	LL_TIM_WriteReg(TIM3, CR1, LL_TIM_ReadReg(TIM3,CR1) | 0x1); // enable counter
 	LL_TIM_WriteReg(TIM3, CNT, 0); // initialize counter
-	int EndCNT = 16800;
 	/* USER CODE END 2 */
 
 	/* Infinite loop */
 	/* USER CODE BEGIN WHILE */
 	while (1) {
-		int CNTStatus = LL_TIM_ReadReg(TIM3, CNT);
-		if (CNTStatus >= EndCNT) {
-			LL_GPIO_WriteReg(GPIOA, ODR,
-					LL_GPIO_ReadReg(GPIOA, ODR) ^ (1U << 10)); // toggles output
-			LL_TIM_WriteReg(TIM3, SR, LL_TIM_ReadReg(TIM3,SR) & ~(0x3)); // clear flags
-			LL_TIM_WriteReg(TIM3, CNT, 0); // resets counter
-		}
+
 		/* USER CODE END WHILE */
 
 		/* USER CODE BEGIN 3 */
+		int StatR = LL_TIM_ReadReg(TIM3, SR);
+		int cc1if = (StatR >> 1) & 0x1; //Ch1
+		int cc2if = (StatR >> 2) & 0x1; //Ch2
+
+		if (cc1if == 1) {
+			LL_TIM_WriteReg(TIM3, SR, LL_TIM_ReadReg(TIM3,SR) & ~(0b010)); // clear flag
+			LL_TIM_WriteReg(TIM3, CCR1,
+					LL_TIM_ReadReg(TIM3, CCR1) + HALF_PERIOD1); //TC1 += HALF_PERIOD1
+		}
+		if (cc2if == 1) {
+			LL_TIM_WriteReg(TIM3, SR, LL_TIM_ReadReg(TIM3,SR) & ~(0b100)); // clear flag
+			LL_TIM_WriteReg(TIM3, CCR2,
+					LL_TIM_ReadReg(TIM3, CCR2) + HALF_PERIOD2); //TC2 += HALF_PERIOD2
+		}
+		//			LL_TIM_WriteReg(TIM3, SR, LL_TIM_ReadReg(TIM3,SR) & ~(0b001)); // clear UIF
 	}
 	/* USER CODE END 3 */
 }
@@ -136,7 +144,7 @@ void SystemClock_Config(void) {
 
 	}
 	LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLLM_DIV_16, 336,
-			LL_RCC_PLLP_DIV_4);
+	LL_RCC_PLLP_DIV_4);
 	LL_RCC_PLL_Enable();
 
 	/* Wait till PLL is ready */
@@ -173,6 +181,8 @@ static void MX_TIM3_Init(void) {
 	LL_TIM_InitTypeDef TIM_InitStruct = { 0 };
 	LL_TIM_OC_InitTypeDef TIM_OC_InitStruct = { 0 };
 
+	LL_GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+
 	/* Peripheral clock enable */
 	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM3);
 
@@ -181,23 +191,39 @@ static void MX_TIM3_Init(void) {
 	/* USER CODE END TIM3_Init 1 */
 	TIM_InitStruct.Prescaler = 0;
 	TIM_InitStruct.CounterMode = LL_TIM_COUNTERMODE_UP;
-	TIM_InitStruct.Autoreload = 33600;
+	TIM_InitStruct.Autoreload = 65535;
 	TIM_InitStruct.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
 	LL_TIM_Init(TIM3, &TIM_InitStruct);
 	LL_TIM_DisableARRPreload(TIM3);
 	LL_TIM_SetClockSource(TIM3, LL_TIM_CLOCKSOURCE_INTERNAL);
-	TIM_OC_InitStruct.OCMode = LL_TIM_OCMODE_FROZEN;
-	TIM_OC_InitStruct.OCState = LL_TIM_OCSTATE_DISABLE;
+	TIM_OC_InitStruct.OCMode = LL_TIM_OCMODE_TOGGLE;
+	TIM_OC_InitStruct.OCState = LL_TIM_OCSTATE_ENABLE;
 	TIM_OC_InitStruct.OCNState = LL_TIM_OCSTATE_DISABLE;
 	TIM_OC_InitStruct.CompareValue = 0;
 	TIM_OC_InitStruct.OCPolarity = LL_TIM_OCPOLARITY_HIGH;
+
 	LL_TIM_OC_Init(TIM3, LL_TIM_CHANNEL_CH1, &TIM_OC_InitStruct);
 	LL_TIM_OC_DisableFast(TIM3, LL_TIM_CHANNEL_CH1);
+	LL_TIM_OC_Init(TIM3, LL_TIM_CHANNEL_CH2, &TIM_OC_InitStruct);
+	LL_TIM_OC_DisableFast(TIM3, LL_TIM_CHANNEL_CH2);
+
 	LL_TIM_SetTriggerOutput(TIM3, LL_TIM_TRGO_RESET);
 	LL_TIM_DisableMasterSlaveMode(TIM3);
 	/* USER CODE BEGIN TIM3_Init 2 */
 
 	/* USER CODE END TIM3_Init 2 */
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+	/**TIM3 GPIO Configuration
+	 PA6   ------> TIM3_CH1
+	 PA7   ------> TIM3_CH2
+	 */
+	GPIO_InitStruct.Pin = LL_GPIO_PIN_6 | LL_GPIO_PIN_7;
+	GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+	GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+	GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+	GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+	GPIO_InitStruct.Alternate = LL_GPIO_AF_2;
+	LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 }
 
@@ -207,24 +233,12 @@ static void MX_TIM3_Init(void) {
  * @retval None
  */
 static void MX_GPIO_Init(void) {
-	LL_GPIO_InitTypeDef GPIO_InitStruct = { 0 };
 	/* USER CODE BEGIN MX_GPIO_Init_1 */
 
 	/* USER CODE END MX_GPIO_Init_1 */
 
 	/* GPIO Ports Clock Enable */
 	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
-
-	/**/
-	LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_10);
-
-	/**/
-	GPIO_InitStruct.Pin = LL_GPIO_PIN_10;
-	GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
-	GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
-	GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-	GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-	LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 	/* USER CODE BEGIN MX_GPIO_Init_2 */
 
